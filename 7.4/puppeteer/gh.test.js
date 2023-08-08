6c1819cd8a15a856629b3e9fcd26588a14b1c9e5
KozachekYury <show_@mail.ru>
2023-08-08 11:36:16 +0300

    puppeteer_1

--- 7.4/puppeteer/gh.test.js
+++ 7.4/puppeteer/gh.test.js
@@ -1,22 +1,73 @@
 let page;
 
-beforeEach(async () => {
-  page = await browser.newPage();
-  await page.goto("https://github.com/team");
+describe("Github Marketplace imgbot page test", () => {
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/marketplace/imgbot");
+  });
+
+  afterEach(() => {
+    page.close();
+  });
+
+  test("The header content text", async () => {
+    const textImgbot = ".f00-light.lh-condensed.mb-3";
+    const actual = await page.$eval(textImgbot, link => link.textContent);
+    expect(actual).toContain("Imgbot");
+  });
+
+  test("The button content text", async () => {
+    const textBtn = ".btn.btn-primary.px-6.py-2.mb-2";
+    const actual = await page.$eval(textBtn, link => link.textContent);
+    expect(actual).toContain("Set up a free trial");
+  });
+
 });
 
-afterEach(() => {
-  page.close();
+describe("Github Marketplace page test", () => {
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/marketplace");
+  });
+
+  afterEach(() => {
+    page.close();
+  });
+
+  test("The header content text", async () => {
+    const textH1 = ".h1.mb-2.lh-condensed-ultra";
+    const actual = await page.$eval(textH1, link => link.textContent);
+    expect(actual).toContain("Extend GitHub");
+  });
+
+  test("The button content text", async () => {
+    const textBtn = ".btn.btn-large.mr-2";
+    const actual = await page.$eval(textBtn, link => link.textContent);
+    expect(actual).toContain("Explore free apps");
+  });
+
 });
 
 describe("Github page tests", () => {
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+    await page.goto("https://github.com/team");
+  });
+  
+  afterEach(() => {
+    page.close();
+  });
+
   test("The h1 header content'", async () => {
     const firstLink = await page.$("header div div a");
     await firstLink.click();
     await page.waitForSelector('h1');
     const title2 = await page.title();
-    expect(title2).toEqual('GitHub: Where the world builds software · GitHub');
-  });
+    expect(title2).toEqual('GitHub: Let’s build from here · GitHub');
+  }, 11000 );
 
   test("The first link attribute", async () => {
     const actual = await page.$eval("a", link => link.getAttribute('href') );
@@ -29,6 +80,7 @@ describe("Github page tests", () => {
       visible: true,
     });
     const actual = await page.$eval(btnSelector, link => link.textContent);
-    expect(actual).toContain("Sign up for free")
+    expect(actual).toContain("Get started with Team")
   });
+  
 });
